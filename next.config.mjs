7d71056e97ece37253f69ec3ee5baf2f900208ad
AdonsFCO <52143660+AdonsFCO@users.noreply.github.com>
2024-06-06 13:06:21 -0400

    fix pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,12 @@
-const withPWA = require('next-pwa')({
-    dest: 'public'
-  })
-
+import withPWA from 'next-pwa';
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {reactStrictMode:true};
+const nextConfig = {
+  reactStrictMode: true,
+  pwa: {
+    dest: 'public',
+    // Add other PWA options here if needed
+  }
+};
 
-module.exports = withPWA({
-    nextConfig
-  })
+export default withPWA(nextConfig);
