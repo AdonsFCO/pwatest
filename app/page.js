0a92221eabf810ac6252d524c6567798edc9fa3a
AdonsFCO <52143660+AdonsFCO@users.noreply.github.com>
2024-06-06 09:24:21 -0400

    redirect to login

--- app/page.js
+++ app/page.js
@@ -1,9 +1,13 @@
 import Image from "next/image";
+import { redirect } from 'next/navigation';
 
-export default function Home() {
+
+export default  async function Home() {
+  redirect('/api/auth/login')
   return (
+    
     <main className="">
-
+        <h1>Hi!</h1>
     </main>
   );
 }
