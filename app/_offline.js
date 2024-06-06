b2fd316f96d47384ab76f3c85e625437045ac039
AdonsFCO <52143660+AdonsFCO@users.noreply.github.com>
2024-06-06 11:29:12 -0400

    manifest fix

---
+++ app/_offline.js
@@ -0,0 +1,9 @@
+// app/_offline.js
+export default function Offline() {
+  return (
+    <div>
+      <h1>You are offline</h1>
+      <p>Sorry, you need an internet connection to view this page.</p>
+    </div>
+  );
+}
