bcc83055f4cc33f36dc8df391c3ecda15e3e9370
AdonsFCO <52143660+AdonsFCO@users.noreply.github.com>
2024-06-06 10:01:49 -0400

    dasboard

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,7 +1,12 @@
-export default function Page() {
-    return (<main>
-        <h1>Hello jorge this is just a test</h1>
-        <a href="/api/auth/logout">Logout</a>
+import { withPageAuthRequired } from "@auth0/nextjs-auth0";
 
-    </main>);
-  }
+const Page = () => {
+  return (
+    <main>
+      <h1>Hello jorge this is just a test</h1>
+      <a href="/api/auth/logout">Logout</a>
+    </main>
+  );
+};
+
+export default withPageAuthRequired(Page);
