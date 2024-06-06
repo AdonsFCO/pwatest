129b68fb443f38fec3311aa0e7d32e45bb7c5721
AdonsFCO <52143660+AdonsFCO@users.noreply.github.com>
2024-06-06 12:15:48 -0400

    fix config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,12 @@
-import withPWA from 'next-pwa';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-const pwaConfig = withPWA({
-  dest: 'public',
-  // Add your PWA config options here
-});
 
-export default {
-  ...nextConfig,
-  ...pwaConfig,
-};
+const withPWA = require('next-pwa')({
+    dest: 'public'
+  })
+  
+  module.exports = withPWA({
+    nextConfig
+  })
+
