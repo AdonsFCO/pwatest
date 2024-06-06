ccc62803d9e4c5d735788aea897d90db0e9c7d22
AdonsFCO <52143660+AdonsFCO@users.noreply.github.com>
2024-06-06 12:57:33 -0400

    no redirect

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import { redirect } from 'next/navigation';
 
 
 export default  async function Home() {
-  redirect('/api/auth/login?returnTo=/dashboard');
+  // redirect('/api/auth/login?returnTo=/dashboard');
   return (
     
     <main className="">
